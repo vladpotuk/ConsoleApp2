78bdca13e9cb15c75bc156bdf1606e8ed7ac7452
vladpotuk <vladpotuk01@gmail.com>
2024-02-20 19:16:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("It's easy to win forgiveness for being wrong;\nbeing right is what gets you into real trouble.");
+        Console.WriteLine("Bjarne Stroustrup");
+    }
+}
